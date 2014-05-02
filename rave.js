e97edc5f71325d04da7c62d46d18fa98f0b51439
unscriptable <john@unscriptable.com>
2014-05-02 17:54:03 -0400

    Update to new rave-amd-plugins API and bump to version 0.1.0.

--- rave.js
+++ rave.js
@@ -7,6 +7,12 @@ var i18n = require('curl/plugin/i18n');
 exports.create = create;
 
 function create (context) {
+	var info = {
+		name: 'curl/plugin/i18n',
+		module: i18n
+	};
 	if (!context.amdPluginMap) context.amdPluginMap = {};
-	context.amdPluginMap.i18n = 'curl/plugin/i18n';
+	context.amdPluginMap['i18n']
+		= context.amdPluginMap['curl/plugin/i18n']
+		= info;
 }
